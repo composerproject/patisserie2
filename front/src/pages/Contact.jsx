22f4c3eeb4cccff1059c4d637d197402e2d8abd0
Luka <luka.lefebvrepro@gmail.com>
2024-05-15 15:47:10 +0200

    Added style, about, contact

---
+++ front/src/pages/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Nous Contacter</h2>
+      <p>
+        Pour nous contacter, veuillez utiliser les informations ci-dessous :
+      </p>
+      <ul>
+        <li>Email: contact@example.com</li>
+        <li>Téléphone: +1 234 567 890</li>
+        <li>Adresse: 123 Rue de l'Exemple, Ville, Pays</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
--- front/src/store/yamsSlice.js
+++ front/src/store/yamsSlice.js
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  dice: Array.from({ length: 5 }, () => Math.floor(Math.random() * 6) + 1),// random initial values
+  dice: Array.from({ length: 5 }, () => Math.floor(Math.random() * 6) + 1), // random initial values
   rollsLeft: 3,
   scores: {
-    suite:0,
+    suite: 0,
     carre: 0,
-    yams:0
+    yams: 0,
   },
-  selectedDice: Array(5).fill(true)
+  selectedDice: Array(5).fill(true),
 };
 
 const yamsSlice = createSlice({
@@ -21,7 +21,7 @@ const yamsSlice = createSlice({
     rollDice: handleDiceRoll,
     resetDiceSelection(state) {
       state.selectedDice.fill(true);
-    }
+    },
   },
 });
 
@@ -30,13 +30,13 @@ function handleDiceRoll(state) {
   state.dice = state.dice.map((value, index) =>
     state.selectedDice[index] ? Math.floor(Math.random() * 6) + 1 : value
   );
-  
+
   // Decrement rolls left
   state.rollsLeft -= 1;
 
   // Calculate scores
   const counts = {};
-  state.dice.forEach(num => counts[num] = (counts[num] || 0) + 1);
+  state.dice.forEach((num) => (counts[num] = (counts[num] || 0) + 1));
 
   if (Object.values(counts).includes(5)) {
     state.scores.yams += 1;
@@ -44,15 +44,17 @@ function handleDiceRoll(state) {
   if (Object.values(counts).includes(4)) {
     state.scores.carre += 1;
   }
-  
+
   const sortedDice = [...state.dice].sort((a, b) => a - b);
-  if (sortedDice.join(',') === '1,2,3,4,5' || sortedDice.join(',') === '2,3,4,5,6') {
+  if (
+    sortedDice.join(",") === "1,2,3,4,5" ||
+    sortedDice.join(",") === "2,3,4,5,6"
+  ) {
     state.scores.suite += 1;
   }
 }
 
-
-
-export const { rollDice, toggleDiceSelection, resetDiceSelection } = yamsSlice.actions;
+export const { rollDice, toggleDiceSelection, resetDiceSelection } =
+  yamsSlice.actions;
 
 export default yamsSlice.reducer;
